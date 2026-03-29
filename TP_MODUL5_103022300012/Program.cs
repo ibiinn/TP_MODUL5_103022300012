e5ed53905cf2f44b6e4b816b1f30ae6a24b84b4e
ibiinn <ibiinn05@gmail.com>
2026-03-29 19:53:32 +0700

    commit message

--- TP_MODUL5_103022300012/Program.cs
+++ TP_MODUL5_103022300012/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+class DataGeneric<T>
+{
+    public T Data { get; set; }
+
+    public DataGeneric(T data)
+    {
+        Data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + Data);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataGeneric<string> data = new DataGeneric<string>("103022300012"); // ganti dengan NIM kamu
+        data.PrintData();
+    }
+}
